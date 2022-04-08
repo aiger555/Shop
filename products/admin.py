a7ee77a73c4725bebdaeed1ab25c7ab1ba7b8aa7
aiger555 <aigerim.nuralieva@iaau.edu.kg>
2022-04-08 18:05:06 +0600

    Added search to BackCall

--- products/admin.py
+++ products/admin.py
@@ -32,7 +32,11 @@ admin.site.register(Advantages)
 admin.site.register(PublicOffers)
 admin.site.register(Help)
 admin.site.register(Footer)
-admin.site.register(BackCall)
 admin.site.register(FloatingButton)
 
 
+@admin.register(BackCall)
+class BackCallAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number', 'created_at', 'type_of_appeal', 'status_of_call')
+    list_filter = ('status_of_call', )
+    search_fields = ('number', 'name', )
